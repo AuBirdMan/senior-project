6aa35668cef824342aac1550a44dda47f1aa60e5
Austin Bird <87907842+AuBirdMan@users.noreply.github.com>
2023-02-03 11:48:42 -0500

    basic battle system

---
+++ A Bird's Dogfight/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+
+public class BattleSystem : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public TMP_Text dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    public BattleState state;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A dogfight has begun!"; //enemyUnit.unitName
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "Your wings smash right into the enemy!";
+        
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        } else
+            {
+                state = BattleState.ENEMYTURN;
+                StartCoroutine(EnemyTurn());
+            }
+    }
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+            {
+                dialogueText.text = "You've defeated Pina!";
+            } else if (state == BattleState.LOST)
+                {
+                    dialogueText.text = "You were devoured.";
+                }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " digs her fangs into you!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if(isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }else
+            {
+                state = BattleState.PLAYERTURN;
+                PlayerTurn();
+            }
+
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        return;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+}
